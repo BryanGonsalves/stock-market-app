e492b898496facb707a71c9a24bfafa26d0375e2
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-05-29 17:49:02 +0400

    Routing add to App.js

--- stock-app/src/App.js
+++ stock-app/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from "./pages/Home";
+import Stocks from "./pages/Stocks";
 
 const App = () => {
   return (
-    <div>
-        Hello World
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/stocks/:symbols' element={<Stocks />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
