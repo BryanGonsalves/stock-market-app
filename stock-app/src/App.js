1c1d6ce1460a5d2a6863f2dbc7e18a60a4ed0bdc
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-05-29 23:06:41 +0400

    Added Header and Slider

--- stock-app/src/App.js
+++ stock-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
 import Home from "./pages/Home";
 import Stocks from "./pages/Stocks";
 
@@ -8,8 +9,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+
+      <Route element={<Layout/>}>
         <Route path='/' element={<Home />}/>
         <Route path='/stocks/:symbols' element={<Stocks />}/>
+      </Route>
       </Routes>
     </BrowserRouter>
   );
