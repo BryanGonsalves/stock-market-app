3e3fc892bce15363664f6fa12efb8f9a5585c30f
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-05-30 08:40:43 +0400

    Shuffle stock list (Cards.js)

--- stock-app/src/components/Header.js
+++ stock-app/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="navbar header">
             <div className='navbar-header header-center'>
-                <Link to="/" style={{ textDecoration: 'none' }}><h3 className='header-title'>Stock Market App</h3></Link>            
+                <Link to="/" style={{ textDecoration: 'none' }}><h3 className='header-title'>Stock Market Application</h3></Link>            
             </div>
         </div>
     )
