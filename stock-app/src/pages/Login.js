4339f95db772556eb4ede26af478f4f1fdbf8ec0
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-11-17 14:07:56 +0530

    changes to the login and signup

--- stock-app/src/pages/Login.js
+++ stock-app/src/pages/Login.js
@@ -15,7 +15,7 @@ const Login = ({ setUser }) => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5001/login', {
+      const response = await fetch('https://stock-market-backend-hd5f.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
--- stock-app/src/pages/Signup.js
+++ stock-app/src/pages/Signup.js
@@ -24,7 +24,7 @@ const Signup = () => {
         }
 
         try {
-            const response = await fetch('http://localhost:5001/signup', {
+            const response = await fetch('https://stock-market-backend-hd5f.onrender.com/signup', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, email, password }),
